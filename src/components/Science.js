79990fdd8786fd0949938e886436d897be35375c
Priyanka Patel <priyanka.syd@gmail.com>
2021-05-14 15:45:26 +1000

    science-component

--- src/components/Science.js
+++ src/components/Science.js
@@ -1,8 +1,26 @@
 import React, { Component } from "react"; // destructuring
+import axios from 'axios';
 
 class Science extends Component {
+  constructor() {
+    super()
+    this.state ={
+
+    };
+    this.scienceInfo = this.scienceInfo.bind(this)
+  }
+
+  scienceInfo() {
+    const scienceURL = "http://newsapi.org/v2/top-headlines?country=au&category=science&apikey=a598dbb0d4a24ccf8c3a54a403b3e1ce"
+
+  });
+}
   render() {
-    return <h1>Science!</h1>;
+    return (
+      <div>
+      <h1>Science News!</h1>;
+      </div>
+    );
   }
 }
 
