b54fdc48bca633bf408e605471c32fa1c5483782
Mauritz Erick <mauritzerick@192-168-1-101.tpgi.com.au>
2021-05-14 13:04:13 +1000

    changed repo name

---
+++ src/components/Homepage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'; // destructuring
+
+class Homepage extends Component {
+  render() {
+    return (
+        <h1>Hello Homepage!</h1>
+    );
+  }
+}
+
+export default Homepage; // remember to export
