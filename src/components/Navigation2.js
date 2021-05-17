101f212124591367b4e39c7b9800197e747479f4
Mauritz Erick <mauritzerick@Mauritzs-MacBook-Air.local>
2021-05-17 10:27:52 +1000

    modified navlink

--- src/components/Navigation2.js
+++ src/components/Navigation2.js
@@ -5,13 +5,13 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 const Navigation2 = () => {
   return (
     <Nav>
-      <Nav.Link href="/general">GENERAL</Nav.Link>||
-      <Nav.Link href="/business">BUSINESS</Nav.Link>||
-      <Nav.Link href="/sports">SPORTS</Nav.Link>||
-      <Nav.Link href="/weather">WEATHER</Nav.Link>||
-      <Nav.Link href="/science">SCIENCE</Nav.Link>||
-      <Nav.Link href="/crypto">Crypto</Nav.Link>||
-      <Nav.Link href="/justin">JUSTIN</Nav.Link>
+      <Nav.Link href="/general" className="inactive" activeClassName="active">GENERAL</Nav.Link>||
+      <Nav.Link href="/business" className="inactive" activeClassName="active">BUSINESS</Nav.Link>||
+      <Nav.Link href="/sports" className="inactive" activeClassName="active">SPORTS</Nav.Link>||
+      <Nav.Link href="/weather" className="inactive" activeClassName="active">WEATHER</Nav.Link>||
+      <Nav.Link href="/science" className="inactive" activeClassName="active">SCIENCE</Nav.Link>||
+      <Nav.Link href="/crypto" className="inactive" activeClassName="active">Crypto</Nav.Link>||
+      <Nav.Link href="/justin" className="inactive" activeClassName="active">JUSTIN</Nav.Link>
 
     </Nav>
   );
