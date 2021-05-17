8dc5ab1ae008287236c3becceef3f7cffce40e00
Priyanka Patel <priyanka.syd@gmail.com>
2021-05-17 11:24:58 +1000

    entertainment component added

--- src/components/Navigation2.js
+++ src/components/Navigation2.js
@@ -8,7 +8,7 @@ const Navigation2 = () => {
 
       <Nav.Link href="/news/general" className="inactive" activeClassName="active">GENERAL</Nav.Link>||
       <Nav.Link href="/news/business" className="inactive" activeClassName="active">BUSINESS</Nav.Link>||
-      <Nav.Link href="/news/entertainmnet" className="inactive" activeClassName="active">ENTERTAINMENT</Nav.Link>||
+      <Nav.Link href="/news/entertainment" className="inactive" activeClassName="active">ENTERTAINMENT</Nav.Link>||
       <Nav.Link href="/news/sports" className="inactive" activeClassName="active">SPORTS</Nav.Link>||
       <Nav.Link href="/weather" className="inactive" activeClassName="active">WEATHER</Nav.Link>||
       <Nav.Link href="/news/science" className="inactive" activeClassName="active">SCIENCE</Nav.Link>||
