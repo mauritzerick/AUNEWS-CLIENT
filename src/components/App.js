b3d0e251b1d38442e085644f637a11d962af5fcf
Qiong DAI(Nelly) <daiqiong524@outlook.com>
2021-05-18 14:05:23 +1000

    wearth search

--- src/components/App.js
+++ src/components/App.js
@@ -10,19 +10,14 @@ import Crypto from "./Crypto";
 import "../chart/chart.css";
 // import Apps from "../chart/Apps"
 // import Header from "../chart/comps/Header"
-import CoinSummaryPage from "../chart/pages/CoinSummaryPage"
-import CoinDetailPage from "../chart/pages/CoinDetailPage"
+import CoinSummaryPage from "../chart/pages/CoinSummaryPage";
+import CoinDetailPage from "../chart/pages/CoinDetailPage";
 import { WatchListContextProvider } from "../chart/context/watchListContext";
 import Justin from "./Justin";
 import Search from "./Search";
 import Weathernav from "./Weathernav";
 
-<<<<<<< HEAD
-import { prettyDOM } from "@testing-library/dom";
-=======
-import News from "./News"
-
->>>>>>> a09a560c9df8bbfd6a9f1e44f273f03f54f28c3b
+import News from "./News";
 
 function App() {
   return (
@@ -32,18 +27,13 @@ function App() {
 
         <Navigation2 />
 
-
-
         <Switch>
-<<<<<<< HEAD
-=======
           <Route exact path="/">
             <Homepage />
           </Route>
 
-          <Route exact path="/news/:category" component={News}/>
+          <Route exact path="/news/:category" component={News} />
 
->>>>>>> a09a560c9df8bbfd6a9f1e44f273f03f54f28c3b
           <Route exact path="/search">
             <Search />
           </Route>
@@ -61,23 +51,10 @@ function App() {
           </Route> */}
 
           <WatchListContextProvider>
-
-<<<<<<< HEAD
-          <Route exact path="/weathernav">
-            <Weathernav />
-          </Route>
-
-          <Route exact path="/weather">
-            <Weather />
-          </Route>
-=======
             {/* <Header /> */}
             <Route exact path="/chart" component={CoinSummaryPage} />
             <Route path="/coins/:id" component={CoinDetailPage} />
->>>>>>> a09a560c9df8bbfd6a9f1e44f273f03f54f28c3b
-
           </WatchListContextProvider>
-
 
           <Route exact path="/justin">
             <Justin />
--- src/components/Navigation1.js
+++ src/components/Navigation1.js
@@ -7,15 +7,9 @@ const Navigation1 = () => {
   return (
     <Container>
       <Nav>
-<<<<<<< HEAD
-        <Nav.Link href="/">ABC NEWS</Nav.Link>||
-        <Nav.Link href="/weathernav">local weather</Nav.Link>||
-        <Nav.Link href="/search">Search</Nav.Link>
-=======
         <Nav.Link href="/" className="inactive" activeClassName="active" >ABC NEWS</Nav.Link>||
         <Nav.Link href="/weather" className="inactive" activeClassName="active" >WEATHER</Nav.Link>||
         <Nav.Link href="/search" className="inactive" activeClassName="active" >Search</Nav.Link>
->>>>>>> a09a560c9df8bbfd6a9f1e44f273f03f54f28c3b
       </Nav>
     </Container>
   );
